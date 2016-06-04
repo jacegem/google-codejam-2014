d88935d9a1d97bb55a8145e68777ee202dcb8703
jacegem <jacegem@gmail.com>
2016-06-04 10:46:14 +0900

    init

---
+++ GoogleCodeJam/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCodeJam
+{
+    class Program
+    {
+        static void Main(string[] args){
+            //GCJ2014
+            //Q1_MagicTrick
+            //Q1_MagicTrick magicTrick = new Q1_MagicTrick();
+            //magicTrick.solve();
+            //Q2_CookieClickerAlpha
+            QB_CookieClickerAlpha cca = new QB_CookieClickerAlpha();
+            cca.solve();
+        }
+    }
+}
